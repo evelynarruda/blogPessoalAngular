d5ea31f48d0447b61e818782b142b659371d82db
Evelyn <evelynarrudasilva@gmail.com>
2021-11-12 16:25:09 -0300

    [Task: PUT e DELETE Tema]

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://projectdeployblogpessoal.herokuapp.com/api/v1/usuarios/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://projectdeployblogpessoal.herokuapp.com/api/v1/usuarios/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://projectdeployblogpessoal.herokuapp.com/api/v1/usuarios/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://projectdeployblogpessoal.herokuapp.com/api/v1/usuarios/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://projectdeployblogpessoal.herokuapp.com/api/v1/usuarios/tema/${id}`, this.token)
   }
 
 
